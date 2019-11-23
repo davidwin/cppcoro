0c714e675ae0425713b1f2ed09952424478b27cb
David Windehem <davidwin@outlook.com>
2019-11-23 21:05:07 +0100

    Nested namespace definition

--- include/cppcoro/detail/exception_promise.hpp
+++ include/cppcoro/detail/exception_promise.hpp
@@ -7,53 +7,50 @@
 
 #include <exception>
 
-namespace cppcoro
+namespace cppcoro::detail
 {
-	namespace detail
+	class empty_base
 	{
-		class empty_base
+	};
+
+	template<bool NoExcept, typename Base = empty_base>
+	class exception_promise;
+
+	template<typename Base>
+	class exception_promise<false, Base> : public Base
+	{
+	protected:
+		exception_promise() noexcept = default;
+		exception_promise(const exception_promise&) = delete;
+		exception_promise(exception_promise&&) = delete;
+
+		std::exception_ptr m_exception = nullptr;
+
+	public:
+		void unhandled_exception() noexcept { m_exception = std::current_exception(); }
+
+		void rethrow_if_exception()
 		{
-		};
-
-		template<bool NoExcept, typename Base = empty_base>
-		class exception_promise;
-
-		template<typename Base>
-		class exception_promise<false, Base> : public Base
-		{
-		protected:
-			exception_promise() noexcept = default;
-			exception_promise(const exception_promise&) = delete;
-			exception_promise(exception_promise&&) = delete;
-
-			std::exception_ptr m_exception = nullptr;
-
-		public:
-			void unhandled_exception() noexcept { m_exception = std::current_exception(); }
-
-			void rethrow_if_exception()
+			if (m_exception != nullptr)
 			{
-				if (m_exception != nullptr)
-				{
-					std::rethrow_exception(std::move(m_exception));
-				}
+				std::rethrow_exception(std::move(m_exception));
 			}
-		};
+		}
+	};
 
-		template<typename Base>
-		class exception_promise<true, Base> : public Base
-		{
-		protected:
-			exception_promise() noexcept = default;
-			exception_promise(const exception_promise&) = delete;
-			exception_promise(exception_promise&&) = delete;
+	template<typename Base>
+	class exception_promise<true, Base> : public Base
+	{
+	protected:
+		exception_promise() noexcept = default;
+		exception_promise(const exception_promise&) = delete;
+		exception_promise(exception_promise&&) = delete;
 
-		public:
-			void unhandled_exception() noexcept { std::terminate(); }
+	public:
+		void unhandled_exception() noexcept { std::terminate(); }
 
-			void rethrow_if_exception() noexcept {}
-		};
-	}
+		void rethrow_if_exception() noexcept {}
+	};
 }
 
 #endif
